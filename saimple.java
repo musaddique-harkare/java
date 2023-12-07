0082f2bacc7bb762a7e0b76d3d7b597fb7fd24a3
musaddique-harkare <138776949+musaddique-harkare@users.noreply.github.com>
2023-12-07 22:51:38 +0530

    Update saimple.java
    farhan

--- saimple.java
+++ saimple.java
@@ -1,6 +1,6 @@
 class Simple{
   public static void main(String args[]){
 
-  System.out.println("Welcome to india computer solapur and global shop");
+  System.out.println("Welcome to india computer solapur ,studying with farhan");
   }
 }
