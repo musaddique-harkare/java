9ff13ade579a17189f8875de5a4b2e7706eb4e77
musaddique-harkare <138776949+musaddique-harkare@users.noreply.github.com>
2023-12-07 22:49:13 +0530

    Update saimple.java
    new commit

--- saimple.java
+++ saimple.java
@@ -1,6 +1,6 @@
 class Simple{
   public static void main(String args[]){
 
-  System.out.println("Welcome to india computer");
+  System.out.println("Welcome to india computer solapur and global shop");
   }
 }
